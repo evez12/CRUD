e5bb70950a346be316a5e40650267ee3ddeded21
evez12 <evezhuseynov24@gmail.com>
2024-11-08 23:04:37 +0400

    I created the delete form

--- src/main/java/com/huseynov/crud/controller/EmployeeController.java
+++ src/main/java/com/huseynov/crud/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.huseynov.crud.controller;
 
 import com.huseynov.crud.entity.Employee;
 import com.huseynov.crud.service.EmployeeService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -29,13 +30,13 @@ public class EmployeeController {
     @GetMapping("/add")
     public String addEmployee(Model theModel) {
         theModel.addAttribute("myEmployee", new Employee());
-        return "employees/employee-form";
+        return "employees/employee-add-update-form";
     }
 
-    @GetMapping("/update")
+    @PostMapping("/update")
     public String updateEmployeeForm(@RequestParam("employeeId") int id, Model model) {
         model.addAttribute("myEmployee", employeeService.findById(id));
-        return "employees/employee-form";
+        return "employees/employee-add-update-form";
     }
 
     @PostMapping("/save")
@@ -44,4 +45,22 @@ public class EmployeeController {
         return "redirect:/employees/list";
     }
 
+    // version 1: employee delete
+    @PostMapping("/delete-form")
+    public String deleteEmployee(@RequestParam("employeeId") int id, Model model) {
+        model.addAttribute("deletedEmployee", employeeService.findById(id));
+        return "/employees/employee-delete-form";
+    }
+
+    @PostMapping("/delete")
+    public String deleteEmployee(@ModelAttribute("deletedEmployee") Employee theEmployee) {
+        employeeService.deleteById(theEmployee.getId());
+        return "redirect:/employees/list";
+    }
+
+    // version 2: employee delete
+    @GetMapping("/delete")public String deleteEmployee(@RequestParam("employeeId") int id){
+        employeeService.deleteById(id);
+        return "redirect:/employees/list";
+    }
 }
